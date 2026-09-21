91a060ca5af9ec03a23f864f8307ee875910c66e
amritayurved <rohitsangwan517@gmail.com>
2026-09-21 15:31:38 +0530

    Make main-domain Meta PageView explicit and deduplicated

--- app/layout.tsx
+++ app/layout.tsx
@@ -71,19 +71,20 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
                 s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)
               }(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');
               fbq('init','${META_PIXEL_ID}');
-              fbq('track','PageView');
             `,
           }}
         />
       </head>
       <body>
-        <img
-          height="1"
-          width="1"
-          style={{ display: "none" }}
-          src={`https://www.facebook.com/tr?id=${META_PIXEL_ID}&ev=PageView&noscript=1`}
-          alt=""
-        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src={`https://www.facebook.com/tr?id=${META_PIXEL_ID}&ev=PageView&noscript=1`}
+            alt=""
+          />
+        </noscript>
         {children}
       </body>
     </html>
