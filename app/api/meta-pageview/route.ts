5a98f77448548f11a200fbde409b6be7ac6eab46
amritayurved <rohitsangwan517@gmail.com>
2026-09-24 20:46:49 +0530

    Align Meta PageView route with new Pixel ID

--- app/api/meta-pageview/route.ts
+++ app/api/meta-pageview/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
-const PIXEL_ID = "3143912892480883";
+const PIXEL_ID = "1398113965721936";
 
 function getClientIp(request: NextRequest) {
   const forwarded = request.headers.get("x-forwarded-for");
