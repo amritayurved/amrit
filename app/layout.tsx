41626bcabdd08ef16a9ae5c5f41abaed130977d3
amritayurved <rohitsangwan517@gmail.com>
2026-09-24 12:31:17 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
-@@ -1,4 +1,5 @@
- import type { Metadata } from "next";
+import type { Metadata } from "next";
 +import Script from "next/script";
  import "./globals.css";
  import "./gallery-fix.css";
