09dba00ab17c75875f5c1ed981f5fcfcbfaefd13
amritayurved <rohitsangwan517@gmail.com>
2026-09-25 10:11:22 +0530

    Set main Meta Pixel ID to 3143912892480883

--- app/pixel-test/page.tsx
+++ app/pixel-test/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 
-const META_PIXEL_ID = "849242441547365";
+const META_PIXEL_ID = "3143912892480883";
 
 type MetaWindow = Window & {
   fbq?: ((...args: unknown[]) => void) & { callMethod?: (...args: unknown[]) => void };
