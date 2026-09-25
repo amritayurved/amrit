9d8baa86245a3efde4a1d3cc485c881d3f94755f
amritayurved <rohitsangwan517@gmail.com>
2026-09-25 23:34:44 +0530

    Refactor Meta Pixel Code to use constant for ID
    Updated Meta Pixel Code to use a constant for the pixel ID.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import "./gallery-fix.css";
+import Script from "next/script";
 
 const META_PIXEL_ID = "2952176195138054";
 
@@ -14,34 +15,21 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
     <html lang="hi">
       <head>
         {/* Meta Pixel Code */}
-<Script id="meta-pixel" strategy="afterInteractive">
-  {`
-    !function(f,b,e,v,n,t,s){
-      if(f.fbq)return;
-      n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-      if(!f._fbq)f._fbq=n;
-      n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];
-      t=b.createElement(e);t.async=!0;t.src=v;
-      s=b.getElementsByTagName(e)[0];
-      s.parentNode.insertBefore(t,s)
-    }(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');
-    fbq('init', '2952176195138054');
-    fbq('track', 'PageView');
-  `}
-</Script>
-<noscript>
-  <img
-    height="1"
-    width="1"
-    style={{ display: "none" }}
-    src="https://www.facebook.com/tr?id=2952176195138054&ev=PageView&noscript=1"
-    alt=""
-  />
-</noscript>
-{/* End Meta Pixel Code */}
-      </head>
-      <body>
-        {children}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s){
+              if(f.fbq)return;
+              n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;
+              n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];
+              t=b.createElement(e);t.async=!0;t.src=v;
+              s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)
+            }(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '${META_PIXEL_ID}');
+            fbq('track', 'PageView');
+          `}
+        </Script>
         <noscript>
           <img
             height="1"
@@ -51,6 +39,10 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
             alt=""
           />
         </noscript>
+        {/* End Meta Pixel Code */}
+      </head>
+      <body>
+        {children}
       </body>
     </html>
   );
